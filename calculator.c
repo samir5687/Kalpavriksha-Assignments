39a2b595d06f6679197cc0f3e0e61060630acddf
samir5687 <123239177+samir5687@users.noreply.github.com>
2025-10-08 19:24:51 +0530

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -1,102 +1,131 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
 
-#define MAX 100
+#define MAX_LENGTH 100
 
 
-int precedence(char op) {
-    if (op == '+' || op == '-') return 1;
-    if (op == '*' || op == '/') return 2;
+int myIsDigit(unsigned char ch) {
+    return (ch >= '0' && ch <= '9');
+}
+
+
+int myStrLen(const char *str) {
+    int len = 0;
+    while (str[len] != '\0') len++;
+    return len;
+}
+
+
+void myRemoveNewline(char *str) {
+    int len = myStrLen(str);
+    for (int i = 0; i < len; i++) {
+        if (str[i] == '\n') {
+            str[i] = '\0';
+            break;
+        }
+    }
+}
+
+
+int getPrecedence(char operator) {
+    if (operator == '+' || operator == '-') return 1;
+    if (operator == '*' || operator == '/') return 2;
     return 0;
 }
 
 
-int applyOp(int a, int b, char op, int *error) {
-    switch (op) {
-        case '+': return a + b;
-        case '-': return a - b;
-        case '*': return a * b;
+int performOperation(int operand1, int operand2, char operator, int *errorFlag) {
+    switch (operator) {
+        case '+': return operand1 + operand2;
+        case '-': return operand1 - operand2;
+        case '*': return operand1 * operand2;
         case '/':
-            if (b == 0) {
-                *error = 1;
+            if (operand2 == 0) {
+                *errorFlag = 1; 
                 return 0;
             }
-            return a / b;
+            return operand1 / operand2;
+        default:
+            *errorFlag = 2; 
+            return 0;
     }
-    return 0;
 }
 
 
-int evaluate(char* tokens, int *error) {
-    int i;
-    int values[MAX], valTop = -1;
-    char ops[MAX]; int opTop = -1;
+int evaluateExpression(char *expression, int *errorFlag) {
+    int valueStack[MAX_LENGTH], valueTop = -1;
+    char operatorStack[MAX_LENGTH];
+    int operatorTop = -1;
 
-    for (i = 0; tokens[i] != '\0'; i++) {
-        if (tokens[i] == ' ')
+    for (int index = 0; expression[index] != '\0'; index++) {
+        if (expression[index] == ' ')
             continue;
 
-        
-        else if (isdigit(tokens[i])) {
-            int val = 0;
-            while (i < strlen(tokens) && isdigit(tokens[i])) {
-                val = (val * 10) + (tokens[i] - '0');
-                i++;
-            }
-            values[++valTop] = val;
-            i--;
-        }
-
      
-        else if (tokens[i] == '+' || tokens[i] == '-' ||
-                 tokens[i] == '*' || tokens[i] == '/') {
-            while (opTop != -1 && precedence(ops[opTop]) >= precedence(tokens[i])) {
-                int b = values[valTop--];
-                int a = values[valTop--];
-                char op = ops[opTop--];
-                values[++valTop] = applyOp(a, b, op, error);
-                if (*error) return 0;
+        else if (myIsDigit((unsigned char)expression[index])) {
+            int number = 0;
+            while (index < myStrLen(expression) && myIsDigit((unsigned char)expression[index])) {
+                number = (number * 10) + (expression[index] - '0');
+                index++;
             }
-            ops[++opTop] = tokens[i];
+            valueStack[++valueTop] = number;
+            index--; 
         }
 
        
+        else if (expression[index] == '+' || expression[index] == '-' ||
+                 expression[index] == '*' || expression[index] == '/') {
+
+            while (operatorTop != -1 &&
+                   getPrecedence(operatorStack[operatorTop]) >= getPrecedence(expression[index])) {
+
+                int operand2 = valueStack[valueTop--];
+                int operand1 = valueStack[valueTop--];
+                char operator = operatorStack[operatorTop--];
+
+                valueStack[++valueTop] = performOperation(operand1, operand2, operator, errorFlag);
+                if (*errorFlag) return 0;
+            }
+
+            operatorStack[++operatorTop] = expression[index];
+        }
+
+     
         else {
-            *error = 2;
+            *errorFlag = 2;
             return 0;
         }
     }
 
-    while (opTop != -1) {
-        int b = values[valTop--];
-        int a = values[valTop--];
-        char op = ops[opTop--];
-        values[++valTop] = applyOp(a, b, op, error);
-        if (*error) return 0;
+   
+    while (operatorTop != -1) {
+        int operand2 = valueStack[valueTop--];
+        int operand1 = valueStack[valueTop--];
+        char operator = operatorStack[operatorTop--];
+
+        valueStack[++valueTop] = performOperation(operand1, operand2, operator, errorFlag);
+        if (*errorFlag) return 0;
     }
 
-    return values[valTop];
+    return valueStack[valueTop];
 }
 
 int main() {
-    char expression[MAX];
+    char inputExpression[MAX_LENGTH];
     printf("Enter expression: ");
-    fgets(expression, MAX, stdin);
-    expression[strcspn(expression, "\n")] = '\0';
+    fgets(inputExpression, MAX_LENGTH, stdin);
 
-    int error = 0;
-    int result = evaluate(expression, &error);
+    myRemoveNewline(inputExpression);
 
-    if (error == 1) {
+    int errorFlag = 0;
+    int result = evaluateExpression(inputExpression, &errorFlag);
+
+    if (errorFlag == 1)
         printf("Error: Division by zero.\n");
-    } else if (error == 2) {
+    else if (errorFlag == 2)
         printf("Error: Invalid expression.\n");
-    } else {
+    else
         printf("%d\n", result);
-    }
 
     return 0;
 }
-
