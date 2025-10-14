c2fa398b8c372add95982d4e5504d1321170f9d7
samir5687 <123239177+samir5687@users.noreply.github.com>
2025-10-14 12:39:31 +0530

    Update Performance.c

--- Performance.c
+++ Performance.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 
-
 struct Student {
     int rollNumber;
     char name[50];
@@ -10,7 +9,6 @@ struct Student {
     float averageMarks;
     char grade;
 };
-
 
 float calculateTotal(float m1, float m2, float m3);
 float calculateAverage(float total);
@@ -25,19 +23,29 @@ int main() {
     printf("Enter number of students: ");
     scanf("%d", &numberOfStudents);
 
+ 
+    if (numberOfStudents <= 0) {
+        printf("Invalid number of students. Exiting program.\n");
+        return 0;
+    }
+
     struct Student students[numberOfStudents];
 
-   
     for (i = 0; i < numberOfStudents; i++) {
         printf("\nEnter details for student %d\n", i + 1);
 
-    
         int roll;
         while (1) {
             printf("Roll Number: ");
             scanf("%d", &roll);
+
+            if (roll <= 0) {
+                printf("Invalid roll number .Roll number must be positive.\n");
+                continue;
+            }
+
             if (isDuplicateRoll(students, i, roll)) {
-                printf("Roll number already exists Please enter a different roll number.\n");
+                printf("Roll number already exists. Please enter a different roll number.\n");
             } else {
                 students[i].rollNumber = roll;
                 break;
@@ -47,18 +55,25 @@ int main() {
         printf("Name: ");
         scanf("%s", students[i].name);
 
-        printf("Enter marks in 3 subjects: ");
-        scanf("%f %f %f", &students[i].marks1, &students[i].marks2, &students[i].marks3);
-
        
+        while (1) {
+            printf("Enter marks in 3 subjects: ");
+            scanf("%f %f %f", &students[i].marks1, &students[i].marks2, &students[i].marks3);
+
+            if ((students[i].marks1 < 0 || students[i].marks1 > 100) ||
+                (students[i].marks2 < 0 || students[i].marks2 > 100) ||
+                (students[i].marks3 < 0 || students[i].marks3 > 100)) {
+                printf("Invalid marks entered. Marks should be between 0 and 100.\n");
+            } else {
+                break;
+            }
+        }
+
         students[i].totalMarks = calculateTotal(students[i].marks1, students[i].marks2, students[i].marks3);
         students[i].averageMarks = calculateAverage(students[i].totalMarks);
-
-       
         students[i].grade = assignGrade(students[i].averageMarks);
     }
 
-   
     printf("\nStudent Performance\n");
     for (i = 0; i < numberOfStudents; i++) {
         printf("Roll: %d\n", students[i].rollNumber);
@@ -67,7 +82,6 @@ int main() {
         printf("Average: %.2f\n", students[i].averageMarks);
         printf("Grade: %c\n", students[i].grade);
 
-       
         if (students[i].averageMarks < 35) {
             continue;
         }
@@ -77,7 +91,6 @@ int main() {
         printf("\n");
     }
 
-   
     printf("List of Roll Numbers (via recursion): ");
     printRollNumbersRecursively(students, 0, numberOfStudents);
     printf("\n");
@@ -89,11 +102,9 @@ float calculateTotal(float m1, float m2, float m3) {
     return m1 + m2 + m3;
 }
 
-
 float calculateAverage(float total) {
     return total / 3.0;
 }
-
 
 char assignGrade(float avg) {
     if (avg >= 85)
@@ -107,7 +118,6 @@ char assignGrade(float avg) {
     else
         return 'F';
 }
-
 
 void displayPerformance(char grade) {
     int stars = 0;
@@ -124,11 +134,9 @@ void displayPerformance(char grade) {
     }
 }
 
-
 void printRollNumbersRecursively(struct Student students[], int index, int count) {
     if (index == count)
         return;
-
     printf("%d ", students[index].rollNumber);
     printRollNumbersRecursively(students, index + 1, count);
 }
@@ -136,8 +144,8 @@ void printRollNumbersRecursively(struct Student students[], int index, int count
 int isDuplicateRoll(struct Student students[], int currentIndex, int roll) {
     for (int i = 0; i < currentIndex; i++) {
         if (students[i].rollNumber == roll) {
-            return 1; 
+            return 1;
         }
     }
-    return 0; 
+    return 0;
 }
